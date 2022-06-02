0eb8023c065ac945afd1f410bef91182fc5d4218
keroles295 <kerolesbotros295@gmail.com>
2022-06-01 21:20:55 -0400

    added a git classo4

--- src/com/syntax/class10/MyStudy.java
+++ src/com/syntax/class10/MyStudy.java
@@ -142,17 +142,9 @@ public class MyStudy {
 	    }
 	    System.out.println();
 	    System.out.println();
-	    Scanner input = new Scanner(System.in);
 	    
-	    double[] numbers = new double[5];
-
-	    for (int i = 0; i < numbers.length; i++)
-	    {
-	        System.out.println("Please enter number");
-	        numbers[i] = input.nextDouble();
-	    }
-	    System.out.println(numbers);
-		
+	    
+	    
 	    
 	    
 	    
