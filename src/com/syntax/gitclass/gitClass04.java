d1bb1e0ad68e472218b3a759f6ccc21f20fe6680
keroles295 <kerolesbotros295@gmail.com>
2022-06-01 21:30:52 -0400

    modified the git class04

--- src/com/syntax/gitclass/gitClass04.java
+++ src/com/syntax/gitclass/gitClass04.java
@@ -5,6 +5,7 @@ public class gitClass04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pietro yalaa");
+		System.out.println("Allah Yastate3");
 
 	}
 
